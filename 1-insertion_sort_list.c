123ed186d3a018a346e65e35c8bd5c7927c46c4a
Akos-1 <veronicagyasi@gmail.com>
2023-09-02 12:25:23 -0700

    my insertion

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,43 +1,54 @@
 #include "sort.h"
 /**
- * insertion_sort_list - a doubly linked list of integers is sorted
- * by insertion method
+ * swap_nodes - two nodes in a doubly linked list are swapped
+ * @list: the head of the list's pointer
+ * @node1: First node to be swapped
+ * @node2: Second node to be swapped
+ */
+void swap_nodes(listint_t **list, listint_t *node1, listint_t *node2)
+{
+	if (node1 == node2)
+		return;
+
+	if (node1->prev)
+		node1->prev->next = node2;
+	else
+		*list = node2;
+
+	if (node2->next)
+		node2->next->prev = node1;
+
+	node1->next = node2->next;
+	node2->prev = node1->prev;
+	node2->next = node1;
+
+	if (node1->next)
+		node1->next->prev = node1;
+
+	node1->prev = node2;
+}
+
+/**
+ * insertion_sort_list - a doubly linked list of integers
+ * is sorted by insertion method
  * @list: the head of a doubly-linked list of integers' pointer.
- *
- * Description: the list is printed after each swap.
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *current, *insertion_point, *tmp_prev;
+	listint_t *y, *current, *temp;
 
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
-
-	current = (*list)->next;
-
-	while (current != NULL)
+	for (current = (*list)->next; current != NULL; current = temp)
 	{
-		insertion_point = current->prev;
+		temp = current->next;
+		y = current->prev;
 
-		while (insertion_point != NULL && insertion_point->n > current->n)
+		while (y != NULL && current->n < y->n)
 		{
-			tmp_prev = insertion_point->prev;
-			insertion_point->prev = current;
-			current->next = insertion_point;
-			current->prev = tmp_prev;
-
-			if (tmp_prev != NULL)
-				tmp_prev->next = current;
-			else
-				*list = current;
-
-			insertion_point->next = current->next;
-			if (current->next != NULL)
-				current->next->prev = insertion_point;
-
-			insertion_point = tmp_prev;
+			swap_nodes(list, y, current);
+			print_list(*list);
+			y = current->prev;
 		}
-
-		current = current->next;
 	}
 }
